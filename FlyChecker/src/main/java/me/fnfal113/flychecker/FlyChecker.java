ebfce0fd556ef9047251988e0079b294ee1681e6
FN-FAL113 <88238718+FN-FAL113@users.noreply.github.com>
2021-09-05 21:32:52 +0800

    FlyCheckerv2

--- FlyChecker/src/main/java/me/fnfal113/flychecker/FlyChecker.java
+++ FlyChecker/src/main/java/me/fnfal113/flychecker/FlyChecker.java
@@ -2,7 +2,12 @@ package me.fnfal113.flychecker;
 
 import me.fnfal113.flychecker.Commands.ReloadPlugin;
 import me.fnfal113.flychecker.Listener.FlyListener;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public final class FlyChecker extends JavaPlugin {
 
@@ -10,14 +15,10 @@ public final class FlyChecker extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
-        getConfig().options().copyDefaults();
-        saveDefaultConfig();
-
         System.out.println("IslandFlyChecker has started.\nThis small plugin was developed by FN_FAL113");
 
         getServer().getPluginManager().registerEvents(new FlyListener(this), this);
         getCommand("flychecker").setExecutor(new ReloadPlugin(this));
     }
-
 
 }
--- FlyChecker/src/main/java/me/fnfal113/flychecker/Listener/FlyListener.java
+++ FlyChecker/src/main/java/me/fnfal113/flychecker/Listener/FlyListener.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
 
 
 public class FlyListener implements Listener {
@@ -16,25 +16,19 @@ public class FlyListener implements Listener {
     public FlyListener(FlyChecker plugin) { this.plugin = plugin; }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
-    public void onPermCheck(PlayerMoveEvent event) {
+    public void onPermCheck(PlayerToggleFlightEvent event) {
         Player target = event.getPlayer();
-        String cmi = "cmi.command.fly";
-        String island = "bskyblock.island.fly";
-        String perm1 = this.plugin.getConfig().getString("permission-to-check");
-        String perm2 = this.plugin.getConfig().getString("permission-to-check2");
-        if(!target.isOp() && target.getGameMode() != GameMode.CREATIVE) {
-            if(event.getPlayer().getAllowFlight()){
-                target.setAllowFlight(true);
-            }
-            else if((!(event.getPlayer().hasPermission(perm1)) && (!event.getPlayer().hasPermission(perm2))) && target.isFlying() && perm1.equals(cmi) && perm2.equals(island)) {
+        if(!target.isOp() && target.getGameMode() != GameMode.CREATIVE && target.isFlying()) {
+                if (event.getPlayer().hasPermission("cmi.command.fly") || event.getPlayer().hasPermission("bskyblock.island.fly")) {
+                    target.setAllowFlight(true);
+                    target.setFlying(true);
+                }
+                else {
                     target.setAllowFlight(!target.getAllowFlight());
                     target.setFlying(target.getAllowFlight());
                     target.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "[FlyChecker]" + ChatColor.AQUA + " Your fly has expired.");
-            }
-
+                }
         }
-
-
     }
 
 }
